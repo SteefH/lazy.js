12bfede3b251f144a4bd816af1cddbd530f7808b
Stefan van der Haven <stefan@infi.nl>
2013-01-17 09:50:20 +0100

    Removing cruft

--- src/lazy.js
+++ src/lazy.js
@@ -5,30 +5,16 @@
         define(factory);
     } else {
         root.Lazy = factory();
-  }
+    }
 }(this, function () {
-    var log;
-    function isFunction (x) {
-        return typeof x === 'function';
-    }
-    if (console && isFunction(console.log)) {
-        log = console.log.bind(console);
-    } else {
-        log = function (){ }; // noop
-    }
-    
-    var Lazy = {
-        debug: false,
+    return {
         lazy: function (initializeName, obj) {
             var propertyName, property, wrappedMethods, initFunc;
-            
+
             initFunc = obj[initializeName];
             wrappedMethods = {};
-            
-            function unwrapMethods (obj) {
-                if (Lazy.debug) {
-                     log(['unwrapping methods', wrappedMethods, obj]);
-                }
+
+            function unwrapMethods(obj) {
                 var methodName;
                 obj[initializeName] = initFunc;
                 for (methodName in wrappedMethods) {
@@ -37,39 +23,26 @@
                     }
                 }
             }
-            
+
             function wrapMethod(wrappedMethodName, method) {
-                if (Lazy.debug) {
-                    log(['wrapping method ' + wrappedMethodName, obj]);
-                }
                 wrappedMethods[wrappedMethodName] = method;
                 return function () {
-                    if (Lazy.debug) {
-                        log(['calling wrapped method ' + wrappedMethodName, obj]);
-                    }
                     unwrapMethods(this);
                     initFunc.apply(this);
                     return method.apply(this, arguments);
                 };
             }
-            
+
             function wrapInitFunc() {
-                return function () {
-                    if (Lazy.debug) {
-                        log(['noop init', obj]);
-                    }
-                };
+                return function () { /* noop */ };
             }
-            
-            
+
             for (propertyName in obj) {
-                if (
-                    propertyName === initializeName
-                ) {
+                if (propertyName === initializeName) {
                     obj[propertyName] = wrapInitFunc();
                 } else {
                     property = obj[propertyName];
-                    if (isFunction(property)) {
+                    if (typeof property === 'function') {
                         obj[propertyName] = wrapMethod(propertyName, property);
                     }
                 }
@@ -77,7 +50,4 @@
             return obj;
         }
     };
-    return Lazy;
 }));
-
-
